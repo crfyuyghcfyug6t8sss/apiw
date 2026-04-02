dd908a0bde7c4a621f2a8cdadb142ca50ef4227d
crfyuyghcfyug6t8sss <kozexyt@gmail.com>
2026-04-02 18:57:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // ─── DB Setup ────────────────────────────────────────────────────────────────
 const db = new Database('./saas.db');
